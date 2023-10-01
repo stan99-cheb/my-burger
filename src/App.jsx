9f9c74542421a511e4d88f6fd63b6f81dc99d372
stan99 <stan99-cheb@yandex.ru>
2023-10-02 00:24:53 +0300

    добавление маршрутизации

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header/header'
+import RootLayout from './pages/RootLayout/root-layout'
+import ConstructorPage from './pages/ConstructorPage/constructor-page'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true, element: <ConstructorPage />,
+      },
+    ],
+  },
+])
 
 const App = () => {
-
   return (
-    <Header />
+    <RouterProvider router={router} />
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/ConstructorPage/constructor-page.jsx
@@ -0,0 +1,16 @@
+import styles from './constructor-page.module.css'
+
+const ConstructorPage = () => {
+  return (
+    <main className={styles.main}>
+      <div>
+        Левая панель
+      </div>
+      <div>
+        Правая панель
+      </div>
+    </main>
+  );
+}
+
+export default ConstructorPage;
